36fa83bc30e3897facec8da55fec2385d0e6cf60
Ryan Bibby <r.bibby@sheffield.ac.uk>
2018-03-09 09:20:55 +0000

    Updated module code

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-%w(com3300).each do |username|
+%w(com3301).each do |username|
   account = User.where(username: username).first_or_create!(password: 'Gf4E07Z4', password_confirmation: 'Gf4E07Z4')
 end
